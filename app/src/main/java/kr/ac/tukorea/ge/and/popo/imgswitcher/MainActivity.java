f554c6c7c76f7253c8aff163bf9355bb328d964f
popopocorn <popcorn375@naver.com>
2025-03-24 17:11:07 +0900

    완

--- app/src/main/java/kr/ac/tukorea/ge/and/popo/imgswitcher/MainActivity.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/popo/imgswitcher/MainActivity.java
@@ -14,6 +14,7 @@ import kr.ac.tukorea.ge.and.popo.imgswitcher.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private @NonNull ActivityMainBinding binding;
+    private int page = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +27,25 @@ public class MainActivity extends AppCompatActivity {
     private static final int[] img_res={
             R.mipmap.cat1, R.mipmap.cat2, R.mipmap.cat3, R.mipmap.cat4, R.mipmap.cat5,
     };
-    public void onBtnPrev(View view) {
-        int page=2;
+    public void onBtnNext(View view) {
+        if(page<5){
+            ++page;
+        }else{
+            page = 1;
+        }
         int resId=img_res[page-1];
         binding.mainImageView.setImageResource(resId);
+        binding.pageTextView.setText(Integer.toString(page));
     }
 
-    public void onBtnNext(View view) {
-
+    public void onBtnPrev(View view) {
+        if(page > 1){
+            --page;
+        }else{
+            page = 5;
+        }
+        int resId=img_res[page-1];
+        binding.mainImageView.setImageResource(resId);
+        binding.pageTextView.setText(Integer.toString(page));
     }
 }
